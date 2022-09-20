29df73cc62aef6c775d856dd594ff2f53647839c
Fahim Morshed <famambission491@gmail.com>
2022-09-20 13:02:51 +1000

    added few JS logic

--- clickQueuehtml/script.js
+++ clickQueuehtml/script.js
@@ -1,6 +1,10 @@
 //"event.target" - returns html part of the clicked portion
+const clickTile = [];
 document.addEventListener('click', function () {
-    console.log(event);
-    console.log(event.target);
-
+    // console.log(event);
+    // console.log(event.target);
+    if (!event.target.classList.contains('tile')) return; //only add the div that has specific class
+    event.target.classList.add('selected'); //add 'selected' class on clicked element
+    clickTile.push(event.target);
+    console.log(clickTile);
 })
