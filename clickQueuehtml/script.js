9c5199216eb3804a6049a60b15b9095063ece413
Fahim Morshed <famambission491@gmail.com>
2022-09-21 14:01:40 +1000

    added JS and html code

--- clickQueuehtml/script.js
+++ clickQueuehtml/script.js
@@ -4,7 +4,17 @@ document.addEventListener('click', function () {
     // console.log(event);
     // console.log(event.target);
     if (!event.target.classList.contains('tile')) return; //only add the div that has specific class
+    if (clickTile.includes(event.target)) return; //repeat click will not add
     event.target.classList.add('selected'); //add 'selected' class on clicked element
     clickTile.push(event.target);
-    console.log(clickTile);
+
+    if (clickTile.length >= 9) {
+        const interval = setInterval(() => {
+            const delTile = clickTile.shift(); //deletes first element of array
+            delTile.classList.remove('selected');
+            if (clickTile == 0) {
+                clearInterval(interval);
+            }
+        }, 1000);
+    }
 })
