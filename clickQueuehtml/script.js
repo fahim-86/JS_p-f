95d4a5d3e381a80fb25f816df6d500f5a377bd0f
Fahim Morshed <famambission491@gmail.com>
2022-09-22 15:29:04 +1000

    div is adding dynamically inside html through JS,

--- clickQueuehtml/script.js
+++ clickQueuehtml/script.js
@@ -1,8 +1,23 @@
 //"event.target" - returns html part of the clicked portion
-const numberOfTiles = document.querySelectorAll(".tile").length; //will return the number of element exist for the given classname OR document.getElementsByClassname('tile')
+// const numberOfTiles = document.querySelectorAll(".tile");//will return the number of element exist for the given classname. Also, document.getElementsByClassname('tile') returns same but array.
 // console.log(numberOfTiles.length); // it will return 0 if 'defer' isn't use for JS load
-
+const section = document.getElementsByTagName('section')[0];
+const tileNo = 2;
+const numberOfTiles = tileNo ** 2;
 const clickdTile = [];
+
+for (let row = 0; row < tileNo; row++) {
+    //this loop will create <div class="wrapper"></div>
+    const wrapper = document.createElement("div"); //create div element
+    wrapper.classList.add("wrapper"); //adding wrapper class to the element
+    for (let col = 0; col < tileNo; col++) {
+        //this will create <div class="tile"></div>
+        const tile = document.createElement("div");
+        tile.classList.add("tile");
+        wrapper.appendChild(tile); //adding tile div as a child inside wrapper div
+    }
+    section.appendChild(wrapper); //adding wrapper div inside section as a child
+}
 document.addEventListener('click', function () {
     // console.log(event);
     // console.log(event.target);
